fd76b6987adc97f3fed3ee2405857ca7b40cc2c2
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-15 11:00:10 +0000

    Add area method to Square

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Square class with size validation and area computation."""
+
+
+class Square:
+    """Square class with a private size attribute."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
