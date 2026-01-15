fadd7d4c07dd35ca45b11a10ec1a033e6384073f
Ziya Allahverdiyev <zallahve@uwaterloo.ca>
2026-01-15 10:55:44 +0000

    Add Square class with private size attribute

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Square class with a private size attribute."""
+
+    def __init__(self, size):
+        self.__size = size
